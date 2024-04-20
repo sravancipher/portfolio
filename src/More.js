8761885028e6e855613c9c0ab7ddfa08add7eec6
sravancipher <sravanpotnuru24@gmail.com>
2024-04-20 11:37:14 +0530

    updated

--- src/More.js
+++ src/More.js
@@ -33,7 +33,7 @@ function More(props) {
                 </div>
             </div>
             <br />
-            <Divider>
+            <Divider sx={{border:"2px solid "}}>
                 <Chip label="Back" size="large" onClick={() => {props.fn()}} sx={{bgcolor:"red"}}/>
                 <br />
             </Divider>
