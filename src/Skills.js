4ef109adcc26ba2b4654818baffc4bc78f006377
sravancipher <sravanpotnuru24@gmail.com>
2024-04-21 09:26:11 +0530

    project updated

--- src/Skills.js
+++ src/Skills.js
@@ -1,55 +1,19 @@
 function Skills() {
+    let skills=["HTML","CSS","Bootstrap","JavaScript","ReactJs","NodeJs","Java","MySQL"]
     return (
 
         <>
-            <span><b>HTML</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
+        {
+            skills.map((skill,index)=>{
+                return <div key={index}> <span><b>{skill}</b></span>
+                <span >
+                    <div class="progress" style={{ height: "20px" }}>
+                        <div class="progress-bar bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
+                    </div>
+                </span>
                 </div>
-            </span>
-            <span><b>CSS</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
-                </div>
-            </span>
-            <span><b>Bootstrap</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
-                </div>
-            </span>
-            <span><b>JavaScript</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
-                </div>
-            </span>
-            <span><b>React Js</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
-                </div>
-            </span>
-            <span><b>Node Js</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
-                </div>
-            </span>
-            <span><b>Java</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
-                </div>
-            </span>
-            <span><b>MySQL</b></span>
-            <span >
-                <div class="progress" style={{ height: "20px" }}>
-                    <div class="progress-bar-animated bg-warning text-center" style={{ width: "100%" }}><b>100%</b></div>
-                </div>
-            </span>
+            })
+        }
         </>
     )
 }
