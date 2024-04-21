93b246ab7dededfd6e9d53e8c546da70d0a70ef5
sravancipher <sravanpotnuru24@gmail.com>
2024-04-21 12:06:31 +0530

    project updated

--- src/About.js
+++ src/About.js
@@ -28,8 +28,8 @@ function About({fn}) {
         <>
             <div className="container mt-5"  data-aos="fade-right" data-aos-duration="2000" style={{ height: "650px" }}>
                 <div className="row ">
-                    <div className="col-sm-12 col-md-6">
-                        <div class="card card1" style={{ width: "400px" }}>
+                    <div className="col-12 col-md-6">
+                        <div className="card card1" style={{ width: "400px" }}>
                             <img className="card-img-top img-responsive" src="profile1.jpeg" alt="Card image" style={{ width: "100%", height: "300px" }} />
                             <div class="card-body">
                                 <h4 class="card-title">POTNURU SRAVANKUMAR</h4>
@@ -41,7 +41,7 @@ function About({fn}) {
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm-12 col-md-6  align-content-center " >
+                    <div className="col-12 col-md-6  align-content-center " >
                              <Contact fn={fn}/>
                     </div>
                 </div>
