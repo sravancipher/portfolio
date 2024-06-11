42ebbb082081c4b7ab0b9a99721e28828fbfac4b
sravancipher <sravanpotnuru24@gmail.com>
2024-06-11 14:33:59 +0530

    updated

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import CopyrightIcon from '@mui/icons-material/Copyright';
 function Footer(){
     return(
         <>
-          <span className='w-100 d-block m-auto text-center'><CopyrightIcon/> {new Date().getFullYear()} Designed By SravanKumar Potnuru</span>
+          <span className='w-100 d-block m-auto'><CopyrightIcon/> {new Date().getFullYear()} Designed By SravanKumar Potnuru</span>
         </>
     )
 }
